e6af666f62e58997887aa854a52e7267b431d635
akritigupta-15 <akriti.vesis@gmail.com>
2025-08-13 10:25:19 +0530

    Git testing

---
+++ src/main/java/DailyJournal/DailyJournal/Model/GItTesting.java
@@ -0,0 +1,9 @@
+package DailyJournal.DailyJournal.Model;
+
+public class GItTesting {
+
+    public static void main(String args[])
+    {
+            System.out.println("This my testing of git");
+    }
+}
